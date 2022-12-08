f1248b3a376f4caf464bc14b0817c9954f670a06
Daniel Krol <orblivion@gmail.com>
2022-12-08 16:10:08 -0500

    day 4 part 2 in progress

--- day-4.rs
+++ day-4.rs
@@ -1,9 +1,16 @@
 use std::fs;
 
+#[derive(PartialEq)]
+#[derive(Copy)]
+#[derive(Clone)]
+enum Overlap {
+    None, Full, Partial
+}
+
 fn main() {
     let file_str = fs::read_to_string("day-4.input").expect("Failed to read file");
 
-    let overlaps = file_str.trim().split("\n").filter_map(|line| {
+    let overlaps : Vec<Overlap> = file_str.trim().split("\n").map(|line| {
         let (elf1, elf2) = line.split_at(line.find(",").unwrap());
 
         let elf2 = &elf2[1..];
@@ -19,20 +26,24 @@ fn main() {
         let e1_end = e1_end.to_string().parse::<i32>().unwrap();
         let e2_end = e2_end.to_string().parse::<i32>().unwrap();
 
-        // If they share an endpoint, one surely contains or equals the other
-        if e1_start == e2_start {
-            Some(())
-        } else if e1_end == e2_end {
-            Some(())
+        if (e1_start == e2_start) || (e1_end == e2_end){
+            // If they share an endpoint, one surely contains or equals the other
+            Overlap::Full
         } else if (e1_start < e2_start) == (e1_end > e2_end)  {
-            Some(())
+            // e1_start's direction from e2_start is the opposite from the respective direction of
+            // e1_end and e2_end. This means one fully includes the other.
+            Overlap::Full
+        } else if (e1_start < e2_end) || (e2_start < e1_end) {
+            // One is strictly before the other. No overlap.
+            Overlap::None
         } else {
-            // Since start and end times aren't respectively equal, we can ignore <= or => possibilities
-            // So let's check that the order of the starts is the opposite of the order of the ends
-            None
+            // The only remaining possiblity is a partial overlap
+            Overlap::Partial
         }
-    })
-    .count();
+    }).collect();
 
-    println!("Overlaps: {}", overlaps)
+    let num_fulls = overlaps.iter().filter_map(|&overlap| match overlap{Overlap::Full => Some(()), _ => None}).count();
+    let num_partials = overlaps.iter().filter_map(|&overlap| match overlap{Overlap::Partial => Some(()), _ => None}).count();
+
+    println!("Overlaps: full={} partial={}", num_fulls, num_partials)
 }
