c913662d84d27bb4e663a68d3383aeeb03a7318e
Daniel Krol <orblivion@gmail.com>
2022-12-13 15:28:15 -0500

    Works for both lengths of markers! Day 6 part 2 done.

--- day-6.rs
+++ day-6.rs
@@ -4,23 +4,25 @@ use std::collections::VecDeque;
 
 fn main() {
 
-    let file_str = fs::read_to_string("day-6.input").expect("Failed to read file");
+    for marker_length in vec![4, 14] {
+        let file_str = fs::read_to_string("day-6.input").expect("Failed to read file");
 
-    let mut marker : VecDeque<char> = VecDeque::new();
-    let mut index : usize = 0;
+        let mut marker : VecDeque<char> = VecDeque::new();
+        let mut index : usize = 0;
 
-    for (i, ch) in file_str.trim().chars().enumerate() {
-        marker.push_front(ch);
-        if marker.len() > 4 {
-            marker.pop_back();
+        for (i, ch) in file_str.trim().chars().enumerate() {
+            marker.push_front(ch);
+            if marker.len() > marker_length {
+                marker.pop_back();
+            }
+
+            let marker_uniq : HashSet<char> = marker.iter().map(|&x| x).collect();
+            if marker_uniq.len() == marker_length {
+                index = i + 1;
+                break;
+            }
         }
 
-        let marker_uniq : HashSet<char> = marker.iter().map(|&x| x).collect();
-        if marker_uniq.len() == 4 {
-            index = i + 1;
-            break;
-        }
+        println!("{:?}", index);
     }
-
-    println!("{:?}", index);
 }
