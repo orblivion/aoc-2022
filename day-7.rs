350ff8b49069df6a22cb7563f448011f0e2b9228
Daniel Krol <orblivion@gmail.com>
2022-12-19 20:30:14 -0500

    more wip

--- day-7.rs
+++ day-7.rs
@@ -4,7 +4,6 @@ use std::collections::HashMap;
 struct Dir<'a> {
     name: &'a str,
     contents: HashMap<&'a str, FSObj<'a>>,
-    parent: Option<&'a mut Dir<'a>>,
 }
 
 struct File<'a> {
@@ -20,53 +19,54 @@ enum FSObj<'a> {
 fn main() {
     let file_str = fs::read_to_string("day-7.input").expect("Failed to read file");
 
-    let mut root = Dir {
+    let root = Dir {
         name: "",
         contents: HashMap::new(),
-        parent: None
     };
 
-    let mut cur_dir = & mut root;
+    let mut dir_stack = vec![&root];
 
     for line in file_str.trim().split('\n') {
         match line.trim().split(' ').collect::<Vec<&str>>()[..] {
             // Don't care! The only non-command output is the output of ls so we know where it came from.
             ["$", "ls"] => (),
 
-            ["$", "cd", "/"] => cur_dir = & mut root,
+            ["$", "cd", "/"] => dir_stack = vec![&root],
 
             ["$", "cd", ".."] => {
-                match cur_dir.parent {
-                    Some(parent) => cur_dir = parent,
-                    None => {
-                        println!("Can't get to the parent of root");
-                        break;
-                    }
+                dir_stack.pop();
+                if dir_stack.len() == 0 {
+                    println!("Can't get to the parent of root");
+                    break;
                 }
             },
 
             ["$", "cd", dir_name] => {
-                if !cur_dir.contents.contains_key(&dir_name) {
-                    cur_dir.contents.insert(dir_name, FSObj::Dir(Dir {
+                let len = dir_stack.len();
+                let contents = &mut dir_stack.get_mut(len - 1).unwrap().contents;
+                if !contents.contains_key(&dir_name) {
+                    contents.insert(dir_name, FSObj::Dir(Dir {
                         name: dir_name,
                         contents: HashMap::new(),
-                        parent: Some(cur_dir),
                     }));
                 };
-                match cur_dir.contents[dir_name] {
-                    FSObj::Dir(cur_dir) => cur_dir.contents[dir_name],
+                let new_dir = match &contents[dir_name] {
+                    FSObj::Dir(dir) => dir,
                     _ => {
                         // Could happen if it was declared as a file in a previous run
                         println!("Trying to cd into a file: {}", line);
                         break;
                     }
                 };
+                dir_stack.push(&new_dir)
             },
 
             // Don't care! We'll get the dir info when we cd into it and then ls
             ["dir", _] => (),
 
             [size, file_name] => {
+                let len = dir_stack.len();
+                let cur_dir = &mut dir_stack[len - 1];
                 match size.to_string().parse::<u32>() {
                     Ok(size) => {
                         if !cur_dir.contents.contains_key(&file_name) {
