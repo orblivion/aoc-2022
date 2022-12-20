ba64f60ea83ec9d4349bb82763d93ac54f055f25
Daniel Krol <orblivion@gmail.com>
2022-12-20 16:48:56 -0500

    Gives an answer but it's wrong. (too high)

--- day-7.rs
+++ day-7.rs
@@ -1,17 +1,21 @@
 use std::fs;
 use std::collections::HashMap;
 
+#[derive(Debug)]
 struct Dir<'a> {
     // TODO name is implicit in the key into contents of the parent Dir. Probably remove it.
     name: &'a str,
     contents: HashMap<&'a str, FSObj<'a>>,
 }
 
+#[derive(Debug)]
 struct File<'a> {
+    // TODO maybe get rid of this name too?
     name: &'a str,
     size: u32,
 }
 
+#[derive(Debug)]
 enum FSObj<'a> {
     Dir(Dir<'a>),
     File(File<'a>),
@@ -28,6 +32,10 @@ fn main() {
     let mut commands = file_str.trim().split('\n');
 
     build(&mut root, &mut commands, 0);
+
+    let (total, accumulated) = sum_small(&root);
+
+    println!("total: {} accumulated: {}", total, accumulated)
 }
 
 // Returns whether we are doing `cd /`
@@ -44,18 +52,24 @@ fn build<'a>(dir : &mut Dir<'a>, commands : &mut impl Iterator<Item = &'a str>, 
             ["$", "ls"] => (),
 
             ["$", "cd", "/"] => {
-                return true; // signal that we want to unravel the recursion back to root
+                // println!("cd /");
+                if level > 0 {
+                    return true; // signal that we want to unravel the recursion back to root
+                }
+                // If we're at level 0, we're already where we want to be; don't do anything
             }
 
             ["$", "cd", ".."] => {
+                // println!("cd..");
                 if level == 0 {
                     println!("Can't get to the parent of the root directory");
-                    break;
+                    break; // TODO return some other value indicating error
                 }
-                return false; // signal that we only want to go up one directory
+                return false;
             },
 
             ["$", "cd", dir_name] => {
+                // println!("cd {}", dir_name);
                 // If this is our first visit into this directory, create it.
                 if !dir.contents.contains_key(&dir_name) {
                     dir.contents.insert(dir_name, FSObj::Dir(Dir {
@@ -82,6 +96,7 @@ fn build<'a>(dir : &mut Dir<'a>, commands : &mut impl Iterator<Item = &'a str>, 
             ["dir", _] => (),
 
             [size, file_name] => {
+                // println!("file size");
                 match size.to_string().parse::<u32>() {
                     Ok(size) => {
                         if !dir.contents.contains_key(&file_name) {
@@ -99,10 +114,30 @@ fn build<'a>(dir : &mut Dir<'a>, commands : &mut impl Iterator<Item = &'a str>, 
 
             _ => {
                 println!("Unknown command: {}", line);
-                break;
+                break; // TODO return some other value indicating error
             }
         }
     }
 
     return false // end of commands
+}
+
+fn sum_small<'a>(dir : &Dir<'a>) -> (u32, u32) {
+    let mut total : u32 = 0;
+    let mut accum : u32 = 0;
+
+    for (name, fs_obj) in dir.contents.iter() {
+        match fs_obj {
+            FSObj::Dir(next_dir) => {
+                let (this_total, this_accum) = sum_small(next_dir);
+                total += this_total;
+                accum += this_accum;
+            },
+            FSObj::File(file) => {
+                total += file.size;
+            },
+        }
+    }
+
+    return (total, accum + total)
 }
