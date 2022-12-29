969afa85776b16a1c7a94335e3c20bcc32f436dd
Daniel Krol <orblivion@gmail.com>
2022-12-29 10:52:12 -0500

    WIP

--- day-11.rs
+++ day-11.rs
@@ -1,92 +1,92 @@
 use std::fs;
 
-type worry_val = i32;
-type monkey_index = usize
+type WorryVal = i32;
+type MonkeyIndex = usize;
 
-type worry_func = fn(worry_val) -> worry_val;
-type monkey_pass_func = fn(worry_val) -> monkey_index;
+type WorryFunc = fn(WorryVal) -> WorryVal;
+type MonkeyPassFunc = fn(WorryVal) -> MonkeyIndex;
 
 struct Monkey {
-    index : monkey_index,
-    items : Vec<worry_val>,
-    pass_func : monkey_pass_func,
-    operation : worry_func,
+    index : MonkeyIndex,
+    items : Vec<WorryVal>,
+    pass_func : MonkeyPassFunc,
+    operation : WorryFunc,
 }
 
-fn read_monkeys(file_str: &str) -> Result<Vec<Monkey>, Err> {
-   let : monkeys = Vec<Monkey>
-   file_str.split("\n\n").enumerate().map(|index, monkey_string|
-        let monkey = Monkey::read()?
-        assert!(monkey.index == index, "wrong monkey index on read")
-   )
+fn read_monkeys(file_str: &str) -> Result<Vec<Monkey>, String> {
+    file_str.split("\n\n").enumerate().map(|(index, monkey_string)| {
+        let monkey = Monkey::read(monkey_string)?;
+        assert!(monkey.index == index, "wrong monkey index on read");
+        Ok(monkey)
+    }).collect()
 }
 
-fn relax(worry : worry_val) {
+fn relax(worry : WorryVal) {
     worry /= 3
 }
 
 impl Monkey {
     fn read(monkey_str: &str) -> Result<Self, String> {
-        lines = monkey_str.split('\n').map(str::trim())
+        let lines : Vec<&str> = monkey_str.split('\n').map(str::trim).collect();
 
-        if (lines.len() != 6) {
+        if lines.len() != 6 {
             return Err(["Wrong number of lines:\n\n", monkey_str].join(" "))
         }
 
-        let index = match lines[0].split(' ').map(str::split(':')) {
-            [["Monkey"], [i, ""]] -> i.parse(),
-            _ -> None,
-        }
-        if (index.is_none()) {
-            return Err(["Invalid index line: ", lines[0]].join(" "))
-        }
+        let index = match lines[0].split("Monkey ").collect::<Vec<&str>>()[..] {
+            ["", i] => match i.split(':').collect::<Vec<&str>>()[..] {
+                [i, ""] => i.parse(),
+                _ => Err(["Invalid index line: ", lines[0]].join(" ")),
+            },
+            _ => Err(["Invalid index line: ", lines[0]].join(" ")),
+        };
 
-        let items : Option<Vec<worry_val>> = match line[1].split(": ").map(|a| a.split(', ')) {
-            [["Starting items"], worries] -> worries.map(worry_val::parse()).collect(),
-            _ -> None,
-        }
-        if (items.is_none()) {
+        let items : Option<Vec<WorryVal>> = match lines[1].split(": ").map(|a| a.split(", ")) {
+            [["Starting items"], worries] => worries.map(WorryVal::parse()).collect(),
+            _ => None,
+        };
+        if items.is_none() {
             return Err(["Invalid index line: ", lines[1]].join(" "))
         }
 
-        let operation : Option<worry_func> = match line[2].split("Operation: new = old").map(str::trim).map(|a| a.split(' ')) {
-            [[""], [operator, operand]] -> num->parse().map(|num| match operator {
-                "*" -> Some(|old : worry_val| old * operand),
-                "+" -> Some(|old : worry_val| old + operand),
-                _ -> None,
+        let operation : Option<WorryFunc> = match lines[2].split("Operation: new = old").map(str::trim).map(|a| a.split(' ')) {
+            [[""], [operator, operand]] => operand.parse().map(|num| match operator {
+                "*" => Some(|old : WorryVal| old * operand),
+                "+" => Some(|old : WorryVal| old + operand),
+                _ => None,
             }).flatten()
-        }
-        if (operation.is_none()) {
+        };
+        if operation.is_none() {
             return Err(["Invalid operation: ", lines[2]].join(" "))
         }
 
-        let test_val : Option<worry_val> = match line[3].split("Test: divisible by") {
-            ["", i] -> i.trim().parse(),
-            _ -> None,
-        }
-        if (test_val.is_none()) {
+        let test_val : Option<WorryVal> = match lines[3].split("Test: divisible by") {
+            ["", i] => i.trim().parse(),
+            _ => None,
+        };
+        if test_val.is_none() {
             return Err(["Invalid Test line: ", lines[3]].join(" "))
         }
 
-        let test_success : Option<> = match line[3].split("If true: throw to monkey") {
-            ["", i] -> i.trim().parse(),
-            _ -> None,
-        }
-        if (test_success.is_none()) {
+        let test_success : Option<MonkeyIndex> = match lines[3].split("If true: throw to monkey") {
+            ["", i] => i.trim().parse(),
+            _ => None,
+        };
+        if test_success.is_none() {
             return Err(["Invalid Test line: ", lines[4]].join(" "))
         }
 
-        let test_success : Option<> = match line[3].split("If true: throw to monkey") {
-            ["", i] -> i.trim().parse(),
-            _ -> None,
-        }
-        if (test_success.is_none()) {
+        let test_fail : Option<MonkeyIndex> = match lines[3].split("If false: throw to monkey") {
+            ["", i] => i.trim().parse(),
+            _ => None,
+        };
+        if test_fail.is_none() {
             return Err(["Invalid Test line: ", lines[5]].join(" "))
         }
 
-        let pass_func : monkey_pass_func = test_val.zip(test_success).zip(test_fail).map(
+        let pass_func : MonkeyPassFunc = test_val.zip(test_success).zip(test_fail).map(
             |((test_val, test_success), test_fail)|
-            |worry : worry_val| if worry % test_val == 0 {test_success} else {test_fail}
+            |worry : WorryVal| if worry % test_val == 0 {test_success} else {test_fail}
         );
 
         index.zip(items).zip(operation).zip(pass_func)
@@ -101,8 +101,8 @@ impl Monkey {
 
     fn process_monkey(monkeys : Vec<Monkey>, this_monkey : Monkey) {
         this_monkey.items.for_each(|worry|{
-            worry = this_monkey.worry_func(worry)
-            worry = relax(worry)
+            worry = this_monkey.WorryFunc(worry);
+            worry = relax(worry);
             if this_monkey.test(worry) {
                 monkeys[this_monkey.pass_to_if_success].add_item(worry)
             } else {
@@ -111,18 +111,22 @@ impl Monkey {
         })
     }
 
+    fn monkey_business(monkeys : Vec<Monkey>) -> u32 {
+        // TODO
+    }
+
 }
 
 fn main() {
     let file_str = fs::read_to_string("day-10.input").expect("Failed to read file");
 
-    monkeys = read_monkeys(file_str)
+    let monkeys = read_monkeys(file_str);
 
-    for _round in (..20) {
+    for _round in ..20 {
         for monkey in monkeys {
-            monkey.process(mut &monkeys)
+            monkey.process(&mut monkeys)
         }
     }
 
-    println!("Monkey Business: {}", monkey_business(monkeys))
+    println!("Monkey Business: {}", Monkey::monkey_business(monkeys));
 }
